1a2cc1fa7470a9f121f62dabc2113200c68d56f8
ahmed <ahmedibrahimkenawi@gmail.com>
2020-06-20 01:19:04 +0200

    server add bodyParser, listen for post request

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,31 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const aylien = require('./aylien.js')
 
 const app = express()
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+})
+
+app.post('/api', async function (req, res) {
+    res.send(
+        await aylien.nlp( req.body.url )
+    )
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
