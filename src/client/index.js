cc2fb8571e40f35cd88b7e664126629a33b6f1e0
ahmed <ahmedibrahimkenawi@gmail.com>
2020-06-20 01:27:25 +0200

    import all assests

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,29 @@
-import { checkForName } from './js/nameChecker'
+import { checkForUrl } from './js/urlChecker'
+import { nplRequest } from './js/nplRequest'
 import { handleSubmit } from './js/formHandler'
+import { 
+    displayResult,
+    sendMessage,
+    disableBtn,
+    enableBtn
+} from './js/updateUI'
 
-console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/table.scss'
+import './styles/message.scss'
+
+
+export {
+    handleSubmit,
+    checkForUrl,
+    nplRequest,
+    displayResult,
+    sendMessage,
+    disableBtn,
+    enableBtn
+}
