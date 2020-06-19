d3c8fc4a6333e7f06dd9a41c4beb632e638e52be
ahmed <ahmedibrahimkenawi@gmail.com>
2020-06-20 01:58:54 +0200

    config webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,16 @@
 const path = require('path')
-const webpack = require('webpack')
+
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.min.js',
+        libraryTarget: 'var',
+        library: 'client'
+    },
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
@@ -14,6 +20,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -22,14 +32,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
+        new CleanWebpackPlugin()
     ]
 }
